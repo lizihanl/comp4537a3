84041b58e079328acfd9988db42940e29d0a2620
lizihanl <zli214@my.bcit.ca>
2023-04-06 13:56:29 -0700

    make it working on the server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react'
+import Page from './Page'
+import Pagination from './Pagination';
+import axios from 'axios'
 function App() {
+  const [pokemons, setPokemons] = useState([])
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pokemonsPerPage] = useState(10);
+
+  useEffect(() => {
+    axios.get('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json')
+      .then(res => res.data)
+      .then(res => {
+        setPokemons(res)
+      })
+      .catch(err => console.log("err", err))
+  }, [])
+
+  const indexOfLastRecord = currentPage * pokemonsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - pokemonsPerPage;
+  const currentPokemons = pokemons.slice(indexOfFirstRecord, indexOfLastRecord)
+  const numberOfPages = Math.ceil(pokemons.length / pokemonsPerPage);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      < Page currentPokemons={currentPokemons} currentPage={currentPage} />
+      < Pagination
+        numberOfPages={numberOfPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </>
+  )
 }
 
-export default App;
+export default App
