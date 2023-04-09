425c952bf5021fa829288d1b4732385cce288595
lizihanl <zli214@my.bcit.ca>
2023-04-08 17:59:48 -0700

    link login to pokemon page

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import Page from './Page'
+import React, { useEffect, useState } from 'react';
+import Page from './Page';
 import Pagination from './Pagination';
-import axios from 'axios'
-import Search from "./Search";
-import Result from "./Result";
-//import { useState } from "react";
-
+import axios from 'axios';
+import Search from './Search';
+import Result from './Result';
+import Login from './login';
 
 function App() {
-  const [pokemons, setPokemons] = useState([])
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [pokemons, setPokemons] = useState([]);
 
   const [currentPage, setCurrentPage] = useState(1);
   const [pokemonsPerPage] = useState(10);
 
   const [selectedTypes, setSelectedTypes] = useState([]);
 
-
   useEffect(() => {
-    axios.get('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json')
-      .then(res => res.data)
-      .then(res => {
-        setPokemons(res)
+    axios
+      .get('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json')
+      .then((res) => res.data)
+      .then((res) => {
+        setPokemons(res);
       })
-      .catch(err => console.log("err", err))
-  }, [])
+      .catch((err) => console.log('err', err));
+  }, []);
 
   const indexOfLastRecord = currentPage * pokemonsPerPage;
   const indexOfFirstRecord = indexOfLastRecord - pokemonsPerPage;
-  const currentPokemons = pokemons.slice(indexOfFirstRecord, indexOfLastRecord)
+  const currentPokemons = pokemons.slice(indexOfFirstRecord, indexOfLastRecord);
   const numberOfPages = Math.ceil(pokemons.length / pokemonsPerPage);
+
+  const handleLogin = (user) => {
+    setLoggedInUser(user);
+  };
 
   return (
     <>
-    
-      <Search
-        selectedTypes={selectedTypes}
-        setSelectedTypes={setSelectedTypes}
-      />
-      <Result
-        selectedTypes={selectedTypes}
-      />
-    
-      < Page currentPokemons={currentPokemons} currentPage={currentPage} />
-      < Pagination
-        numberOfPages={numberOfPages}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
+      {loggedInUser ? (
+        <>
+          <Search selectedTypes={selectedTypes} setSelectedTypes={setSelectedTypes} />
+          <Result selectedTypes={selectedTypes} />
+          <Page currentPokemons={currentPokemons} currentPage={currentPage} />
+          <Pagination
+            numberOfPages={numberOfPages}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
+        </>
+      ) : (
+        <Login onLogin={handleLogin} />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/login.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+//import axios from 'axios'
+import './login.css';
+
+
+function Login({ onLogin }) {
+  const [username, setUsername] = useState('user1');
+  const [password, setPassword] = useState('password1');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     // Perform login operation with username and password
+//     // If login is successful, call onLogin with the user object
+//   };
+
+// const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     try {
+//       const response = await axios.post('/api/login', {
+//         username,
+//         password,
+//       });
+//       const user = response.data;
+//       onLogin(user);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+const validCredentials = [
+    { username: 'user1', password: 'password1' },
+    { username: 'user2', password: 'password2' },
+  ];
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const matchingCredential = validCredentials.find(
+        (credential) => credential.username === username && credential.password === password
+      );
+      if (matchingCredential) {
+        onLogin(matchingCredential);
+      } else {
+        console.log('Invalid username or password');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  
+
+  return (
+    
+    <div className="login-container">
+    <form id="loginform" onSubmit={handleSubmit}>
+      <label className='loginlabel'>
+        Username:
+        <input classname="logininput" type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label className='loginlabel'>
+        Password:
+        <input classname="logininput" type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button id="loginbutton" type="submit">Login</button>
+    </form>
+    </div>
+  );
+}
+
+export default Login;
